1d467bb1b5676aab0abaec00cf1fbaab5fa46f51
ixangel <28997069+ixangel@users.noreply.github.com>
2020-07-08 23:01:08 +0700

    competitive "tell latch" coding

--- main.cc
+++ main.cc
@@ -5,10 +5,18 @@
 //                                                                    //
 ////////////////////////////////////////////////////////////////////////
 
+#define COPY_TO_CLIPBOARD // remove if you don't want to copy to clipboard
 #include <iostream>
 #include <string>
 #include <vector>
 #include <iomanip>
+#include <sstream>
+
+#ifdef COPY_TO_CLIPBOARD // non-portable, currently windows only
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
+
 std::vector<int> encode(std::string orig, unsigned char offset, std::string key)
 {
     std::vector<int> ret{};
@@ -19,32 +27,44 @@ std::vector<int> encode(std::string orig, unsigned char offset, std::string key)
             for (auto it = key.begin(); it != key.end(); ++it)
                 orig[i] ^= *it;
 
-        switch (i % 4)
-        {
-        case 0:
+        if (!(i % 4)) {
             if (i) ret.push_back(v1);
-            v1 = 0;
             v1 = ((orig[i] + offset) << 24);
-            break;
-        case 1:
-            v1 |= ((orig[i] + offset) << 16);
-            break;
-        case 2:
-            v1 |= ((orig[i] + offset) << 8);
-            break;
-        case 3:
-            v1 |= (orig[i] + offset);
-            break;
+        } else {
+            v1 |= ((orig[i] + offset) << (24 - 8 * i));
         }
+
     }
     ret.push_back(v1);
     return ret;
 }
 
+#ifdef COPY_TO_CLIPBOARD
+void copy_to_clipboard(std::string data)
+{
+    auto buf = GlobalAlloc(GMEM_MOVEABLE, data.size());
+    memcpy(GlobalLock(buf), data.c_str(), data.size());
+    GlobalUnlock(buf);
+
+    OpenClipboard(nullptr);
+    EmptyClipboard();
+    SetClipboardData(CF_TEXT, buf);
+    CloseClipboard();
+
+    GlobalFree(buf);
+}
+#endif
+
 int main()
 {
     std::string str, key; int offset = -1, i = 0;
-    std::cout << "bloomify.me's bao chot software by angel. copyright (c) 2020 bloomify.me" << std::endl;
+    std::cout << "////////////////////////////////////////////////////////////////////////" << std::endl;
+    std::cout << "//                                                                    //" << std::endl;
+    std::cout << "//                  phan mem b40 ch0t thoi dai 4.0                    //" << std::endl;
+    std::cout << "// written by angel from bloomify.me. Copyright (c) 2020 bloomify.me. //" << std::endl;
+    std::cout << "//                                                                    //" << std::endl;
+    std::cout << "////////////////////////////////////////////////////////////////////////" << std::endl << std::endl;
+
     std::cout << "Type the string you want to encode: ";
     std::getline(std::cin, str);
     std::cout << "Key: ";
@@ -58,33 +78,41 @@ int main()
     }
     auto encoded_vec = encode(str, offset, key);
     auto encoded_key = encode(key, 0, {});
-    std::cout << R"(
-#include <iostream>
+
+    std::stringstream ss;
+    ss << R"(#include <iostream>
 #include <vector>
-
 int main(int, char**) {
-    std::vector<unsigned> v1 { )";
+    std::vector<uint32_t> v1 { )";
     for (i = 0; i < encoded_vec.size(); ++i)
-        std::cout << std::hex << "0x" << encoded_vec.at(i) << (i < encoded_vec.size() - 1 ? "u, " : "u ") << std::dec;
-    std::cout << "}, v2 { ";
+        ss << std::hex << "0x" << encoded_vec.at(i) << (i < encoded_vec.size() - 1 ? "u, " : "u ") << std::dec;
+    ss << "}, v2 { ";
     for (i = 0; i < encoded_key.size(); ++i)
-        std::cout << std::hex << "0x" << encoded_key.at(i) << (i < encoded_key.size() - 1 ? "u, " : "u ") << std::dec;
-    std::cout << "};";
+        ss << std::hex << "0x" << encoded_key.at(i) << (i < encoded_key.size() - 1 ? "u, " : "u ") << std::dec;
+    ss << "};";
 
-    std::cout << R"(
+    ss << R"(
     char c; int i, j;
     for (auto it = v1.begin(); it != v1.end(); ++it) {
         for (i = 0; i < 4; ++i) {
             c = char((*it >> (24 - 8 * i)) - )" << std::hex << "0x" << std::setfill('0') << std::setw(2) << offset << ");" <<
-R"(
+        R"(
             for (auto it2 = v2.rbegin(); it2 != v2.rend(); ++it2) {
                 for (j = 0; j < 4; ++j)
                     c ^= char(*it2 >> (8 * j));
             }
             if ((unsigned char)c < 0x80)
-                std::cout << c;
+                ss << c;
         }
     }
-}
-)";
+})";
+    auto s = ss.str();
+    std::cout << "---------------------------------------------------------------------------------------------------------" << std::endl << std::endl;
+    std::cout << s << std::endl;
+    std::cout << "---------------------------------------------------------------------------------------------------------" << std::endl;
+#ifdef COPY_TO_CLIPBOARD
+    copy_to_clipboard(s);
+    std::cout << "Code content copied to clipboard." << std::endl;
+#endif
+    std::getchar();
 }
